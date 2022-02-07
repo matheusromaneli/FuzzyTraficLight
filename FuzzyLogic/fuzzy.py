b60e9ac60a8135504ec5a8415d6e5b83d8ef5f47
Matheus Lima Romaneli <78664028+matheusromaneli@users.noreply.github.com>
2022-02-07 16:28:39 -0300

    Mudando universo de discurso das variáveis

--- FuzzyLogic/fuzzy.py
+++ FuzzyLogic/fuzzy.py
@@ -11,22 +11,22 @@ import random
 # Variáveis ​​Linguisticas. Termos Linguisticos 
 ### Se Antecedente Então Consequente 
 ### Novos objetos Antecedent / Consequent possuem variáveis ​​de universo e número de associação
-pessoa = ctrl.Antecedent(np.arange(0,1001,1), 'pessoas')
-veiculo = ctrl.Antecedent(np.arange(0,1001,1), 'veiculos')
+pessoa = ctrl.Antecedent(np.arange(0,50,1), 'pessoas')
+veiculo = ctrl.Antecedent(np.arange(0,100,1), 'veiculos')
 
 aberto = ctrl.Consequent(np.arange(0,100,1), 'tempo')
 # Fuzzificação 
-pessoa['r1'] = fuzz.trapmf(pessoa.universe, [0,  0,  5, 10])
-pessoa['r2'] = fuzz.trapmf(pessoa.universe, [5, 10, 15, 20])
-pessoa['r3'] = fuzz.trapmf(pessoa.universe, [15, 20, 25, 30])
-pessoa['r4'] = fuzz.trapmf(pessoa.universe, [25, 30, 35, 40])
-pessoa['r5'] = fuzz.trapmf(pessoa.universe, [35, 40, 1000, 1001])
+pessoa['muito baixo'] = fuzz.trapmf(pessoa.universe, [0,  0,  5, 10])
+pessoa['baixo'] = fuzz.trapmf(pessoa.universe, [5, 10, 15, 20])
+pessoa['medio'] = fuzz.trapmf(pessoa.universe, [15, 20, 25, 30])
+pessoa['alto'] = fuzz.trapmf(pessoa.universe, [25, 30, 35, 40])
+pessoa['muito alto'] = fuzz.trapmf(pessoa.universe, [35, 40, 50, 60])
 
-veiculo['v1'] = fuzz.trapmf(veiculo.universe, [0,0, 5,10])
-veiculo['v2'] = fuzz.trapmf(veiculo.universe, [5, 10, 15, 25])
-veiculo['v3'] = fuzz.trapmf(veiculo.universe, [15,25, 30,40])
-veiculo['v4'] = fuzz.trapmf(veiculo.universe, [30,40, 50,60])
-veiculo['v5'] = fuzz.trapmf(veiculo.universe, [50,60,1000,1001])
+veiculo['muito baixo'] = fuzz.trapmf(veiculo.universe, [0,0, 5,10])
+veiculo['baixo'] = fuzz.trapmf(veiculo.universe, [5, 10, 15, 25])
+veiculo['medio'] = fuzz.trapmf(veiculo.universe, [15,25, 30,40])
+veiculo['alto'] = fuzz.trapmf(veiculo.universe, [30,40, 50,60])
+veiculo['muito alto'] = fuzz.trapmf(veiculo.universe, [50,60,100,101])
 
 ### Uma função de pertinência personalizada pode ser construída de forma interativa com uma API Pythonic
 aberto['Mais Fechado']= fuzz.trimf(aberto.universe, [0,15, 30])
@@ -36,11 +36,11 @@ aberto['Aberto']= fuzz.trimf(aberto.universe, [45,60,75])
 aberto['Mais Aberto'] = fuzz.trimf(aberto.universe, [60,75,100])
 
 # Maquina de Inferência 
-ruleMA  = ctrl.Rule((pessoa['r1'] & (veiculo['v3'] | veiculo['v4'] | veiculo['v5']))| (pessoa['r2'] & (veiculo['v4'] | veiculo['v5'])) | (pessoa['r3'] & veiculo['v5']), aberto['Mais Aberto'])
-ruleA = ctrl.Rule((pessoa['r1'] & veiculo['v2']) | (pessoa['r2'] & veiculo['v3']) | (pessoa['r3'] & veiculo['v4']) | (pessoa['r4'] & veiculo['v5']), aberto['Aberto'])
-ruleE  = ctrl.Rule ((veiculo ['v1'] & pessoa ['r1']) | (veiculo ['v1'] & pessoa ['r2']) | (veiculo ['v2'] & pessoa ['r2']) | (veiculo ['v2'] & pessoa ['r3']) | (veiculo ['v3'] & pessoa ['r3']) | (veiculo ['v3'] & pessoa ['r4']) | (veiculo ['v4'] & pessoa ['r4']) | (veiculo ['v4'] & pessoa ['r5']) | (veiculo ['v5'] & pessoa ['r5']), aberto['Equilibrado'])
-ruleF = ctrl.Rule((pessoa['r3'] & veiculo['v1'])|(pessoa['r4'] & veiculo['v2'])|(pessoa['r5'] & veiculo['v3']), aberto['Fechado'])
-ruleMF  = ctrl.Rule(((pessoa['r4'] | pessoa['r5'])& veiculo['v1']) | (pessoa['r5'] & veiculo['v2']), aberto['Mais Fechado'])
+ruleMA  = ctrl.Rule((pessoa['muito baixo'] & (veiculo['medio'] | veiculo['alto'] | veiculo['muito alto']))| (pessoa['baixo'] & (veiculo['alto'] | veiculo['muito alto'])) | (pessoa['medio'] & veiculo['muito alto']), aberto['Mais Aberto'])
+ruleA = ctrl.Rule((pessoa['muito baixo'] & veiculo['baixo']) | (pessoa['baixo'] & veiculo['medio']) | (pessoa['medio'] & veiculo['alto']) | (pessoa['alto'] & veiculo['muito alto']), aberto['Aberto'])
+ruleE  = ctrl.Rule ((veiculo ['muito baixo'] & pessoa ['muito baixo']) | (veiculo ['muito baixo'] & pessoa ['baixo']) | (veiculo ['baixo'] & pessoa ['baixo']) | (veiculo ['baixo'] & pessoa ['medio']) | (veiculo ['medio'] & pessoa ['medio']) | (veiculo ['medio'] & pessoa ['alto']) | (veiculo ['alto'] & pessoa ['alto']) | (veiculo ['alto'] & pessoa ['muito alto']) | (veiculo ['muito alto'] & pessoa ['muito alto']), aberto['Equilibrado'])
+ruleF = ctrl.Rule((pessoa['medio'] & veiculo['muito baixo'])|(pessoa['alto'] & veiculo['baixo'])|(pessoa['muito alto'] & veiculo['medio']), aberto['Fechado'])
+ruleMF  = ctrl.Rule(((pessoa['alto'] | pessoa['muito alto'])& veiculo['muito baixo']) | (pessoa['muito alto'] & veiculo['baixo']), aberto['Mais Fechado'])
 aberto_ctrl = ctrl.ControlSystem([ruleMA, ruleA, ruleE, ruleF, ruleMF])
 aberto_simulator = ctrl.ControlSystemSimulation(aberto_ctrl)
 
